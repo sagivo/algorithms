d02b80256f7b3ad2554756fc719deeb95f6e97b2
Naim Rajib <naim_cse07@yahoo.com>
2014-12-23 23:44:06 +0600

    Remove aditional comment.

--- binary_search.rb
+++ binary_search.rb
@@ -12,20 +12,12 @@ class BinarySearch
   def search(array, value)
     middle_index = (array.length / 2)
 
-    # is array empty? then no result found
     if array.length == 0
       puts "The Searched Value Doesn't exist in the list."
-      # if array is not empty then check array middle element is equal to search element?
     elsif array[middle_index] == value
       puts 'Found in the list.'
-
-      # else then check if the middle element is greater than search element
-      #       swap lower_index with middle element + 1 and call search method
     elsif array[middle_index] < value
       search(array[middle_index+1, array.length], value)
-
-      #   if middle element less than the search element
-      #       swap higher_index with middle element and call search method
     elsif array[middle_index] > value
       search(array[0, middle_index], value)
     end
