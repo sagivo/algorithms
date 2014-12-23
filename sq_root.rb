37839becdfc7fa49a5b84d168b43bd17725f9bcb
sagivo <sagiv4@gmail.com>
2014-12-22 22:13:48 -0500

    root decimal

--- sq_root.rb
+++ sq_root.rb
@@ -1,13 +1,14 @@
 #https://en.wikipedia.org/wiki/Newton%27s_method
-def sqr_root x, accuracy = 0.0001
+def sqr_root x, accuracy = 0.0001, round = 3
   return 'error' if x < 0
-  a,b = 1,x
+  a,b = 1.to_f,x.to_f
   while b-a > accuracy
     b = (a+b) / 2
     a = x/b
   end
-  b
+  b.round round
 end
 
 p sqr_root 100 #10
 p sqr_root 12 #3
+p sqr_root 16
