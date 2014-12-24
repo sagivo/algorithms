de652561af4f44325f1b0c6cc7b9914831943dc5
sagivo <sagiv4@gmail.com>
2014-12-24 02:23:09 -0500

    palindrome fix

--- min_insertions_for_palindrome.rb
+++ min_insertions_for_palindrome.rb
@@ -2,9 +2,10 @@
 def min_for_palindrome str, left=0, right=nil
   right ||= str.size-1
   return Float::INFINITY if left > right
-  return 0 if left == right or left == right-1
+  return 0 if left == right or (left == right-1 and str[left] == str[right])
+  return 1 if left == right-1
   if str[left] == str[right] then min_for_palindrome(str, left+1, right-1) else [min_for_palindrome(str,left+1, right), min_for_palindrome(str,left,right-1)].min + 1 end
 end
 
 #test
-p min_for_palindrome 'geeks' #3
+p min_for_palindrome 'geek' #2 -> kgeegk
