27d2cc0e3d2e07230e443adc9aee195ad903dee2
sagivo <sagiv4@gmail.com>
2014-12-24 01:57:26 -0500

    refactor print

--- join_sets.rb
+++ join_sets.rb
@@ -1,18 +1,10 @@
 #prints all combinations of join sets 
-def join_sets set, i, str = ''
-  return p str if str.size == set.size
+def join_sets set, i = 0, str = '', result = []
+  return result << str if str.size == set.size
   for item in set[i]    
-    join_sets set, i.succ, str + item.to_s
+    join_sets set, i.succ, str + item.to_s, result
   end
+  result
 end
 
-join_sets [ ['A','B'] , ['C','D'], ['E'] ], 0
-
-=begin
-  
-"ACE"
-"ADE"
-"BCE"
-"BDE"
-  
-=end
+p join_sets [ ['A','B'] , ['C','D'], ['E'] ] #["ACE", "ADE", "BCE", "BDE"]
