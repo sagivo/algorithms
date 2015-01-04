e574a1df42386fce9b1b92859b3e80ed060281ea
sagivo <sagiv4@gmail.com>
2015-01-04 17:05:10 -0500

    fix array size o init

--- counting_sort.rb
+++ counting_sort.rb
@@ -1,10 +1,10 @@
 #all numbers need to be positive
 def counting_sort(a)
   max = a.max
-  c = Array.new(max+1, 0) # initialize counting array with k+1 zero values    
+  c = Array.new(max+2, 0) # initialize counting array with k+1 zero values    
   a.each { |i| c[i] += 1 } # build a histogram / count occurrences of all integer values in array a  
-  (0..max).each { |i| c[i] += c[i-1] } # compute indices for each key
-  b = Array.new(a.length, 0) # initialize output array
+  (0..max).each { |i| c[i] += c[i-1] } # compute indices for each key  
+  b = Array.new(a.size, 0) # initialize output array
   a.each { |i| b[c[i]-1] = i;  c[i] -= 1 }
   b
 end
