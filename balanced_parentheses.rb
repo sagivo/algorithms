53daab1c36020c52b5a871774521aa4d4d52005d
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2015-01-07 22:12:13 +0100

    add balanced parentheses algorithm

---
+++ balanced_parentheses.rb
@@ -0,0 +1,17 @@
+def balanced_parentheses(str)
+  stack = []
+  str.chars.each do |token|
+    case token
+      when '('
+        stack.push '('
+      when ')'
+        if stack.empty? || stack.pop != '('
+          return false
+        end
+    end
+  end
+  stack.empty?
+end
+
+#test
+p balanced_parentheses '( () ( () ) () )' # => true
