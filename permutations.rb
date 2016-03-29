bf0329416bc95398e308ff0cc809f7a2fb221ea6
Sagiv Ofek <sagivo@users.noreply.github.com>
2016-03-28 22:24:27 -0400

    use built in exclusive method

--- permutations.rb
+++ permutations.rb
@@ -1,6 +1,6 @@
 def perm arr, i=0
   p arr if i == arr.size
-  (i..arr.size-1).each do |j|
+  (i...arr.size).each do |j|
     arr[i], arr[j] = arr[j], arr[i]
     perm arr, i+1
     arr[i], arr[j] = arr[j], arr[i]
